d0143b3f6a9f23aed5943ff9a777b02820ae01e7
Rhythm22Sharma <rhythm22sharma@gmail.com>
2024-08-09 16:35:23 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements.
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='mlproject',
+    version='3.11.5',
+    author_email='rhythm22sharma@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
